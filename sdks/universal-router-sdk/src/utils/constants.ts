dd34579bae7f1da893e4a4ab1dc33e07374988cd
Uniswap AI Deployer <gs.node@protofire.io>
2025-10-06 18:34:15 +0200

    feat: update uni-v2 contracts

--- sdks/universal-router-sdk/src/utils/constants.ts
+++ sdks/universal-router-sdk/src/utils/constants.ts
@@ -441,12 +441,12 @@ export const CHAIN_CONFIGS: { [key: number]: ChainConfig } = {
     weth: '0xd3bf53dac106a0290b0483ecbc89d40fcc961f3e',
     routerConfigs: {
       [UniversalRouterVersion.V1_2]: {
-        address: '0x7087d5ae7F8A920F9a84BD760383c00D4B0Eb0Dd',
-        creationBlock: 42146649,
+        address: '0x5fE87847fe20a6C30921620F52B06a4A3740aa61',
+        creationBlock: 42574667,
       },
       [UniversalRouterVersion.V2_0]: {
-        address: '0x7087d5ae7F8A920F9a84BD760383c00D4B0Eb0Dd',
-        creationBlock: 42146649,
+        address: '0x5fE87847fe20a6C30921620F52B06a4A3740aa61',
+        creationBlock: 42574667,
       },
     },
   },
